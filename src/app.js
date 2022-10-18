9ccaaff2df0529466edb04ae0af3db8a4ed08b53
anuragc2001 <chakraborty.anurag01@gmail.com>
2022-10-18 23:53:25 +0530

    added routes to app.js and views/ejs

--- src/app.js
+++ src/app.js
@@ -7,13 +7,45 @@ const port = process.env.PORT || 3000
 
 const app = express();
 
-
-// app.use(express.static(path.join(__dirname, '/../public')))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '/../public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
-    res.send("Hello");
+    res.render('index');
+})
+
+app.get('/templates/why.ejs',(req,res)=>{
+    res.render('why');
+})
+
+app.get('/templates/about.ejs',(req,res)=>{
+    res.render('about');
+})
+
+app.get('/templates/contact.ejs',(req,res)=>{
+    res.render('contact');
+})
+
+app.get('/templates/download.ejs',(req,res)=>{
+    res.render('download');
+})
+
+app.get('/templates/faq.ejs',(req,res)=>{
+    res.render('faq');
+})
+
+app.get('/templates/lofttanks.ejs',(req,res)=>{
+    res.render('lofttanks');
+})
+
+app.get('/templates/plastictanks.ejs',(req,res)=>{
+    res.render('plastictanks');
+})
+
+app.get('/templates/ss.ejs',(req,res)=>{
+    res.render('ss');
 })
 
 app.listen(port, () => {
