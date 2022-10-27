1f230356547933d32394b17c4747bfd78c21a84b
anuragc2001 <chakraborty.anurag01@gmail.com>
2022-10-27 23:20:58 +0530

    admin panel details fetched from backend

--- src/controllers/admin/dashboard.js
+++ src/controllers/admin/dashboard.js
@@ -1,8 +1,26 @@
+const {stainlessSteelTankModel} = require('../../models/tanks.model')
+const {plasticTankModel} = require('../../models/tanks.model')
+const {loftTankModel} = require('../../models/tanks.model')
 
-const dashboard = (req, res) => {
-    console.log(req.User);
-    res.render('admin/index', {u_name: req.User.adminName})
-}
+
+
+const dashboard = (req, res, next) => {
+    let queries = [
+        stainlessSteelTankModel.find({}).limit(6),
+        plasticTankModel.find({}).limit(6),
+        loftTankModel.find({}).limit(6)
+    ]
+    
+    Promise.all(queries)
+        .then((result) => {
+            console.log("index");
+            console.log(req.User);
+            res.render('admin/index', {u_name: req.User.adminName, ss_data: result[0], p_data: result[1], l_data: result[2]})
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+};
 
 module.exports = {
     dashboard
--- src/controllers/admin/plastic.js
+++ src/controllers/admin/plastic.js
@@ -1,8 +1,20 @@
+const {plasticTankModel} = require('../../models/tanks.model')
 
-const plastic = (req, res) => {
-    console.log(req.User);
-    res.render('admin/plastic', {u_name: req.User.adminName})
-}
+const plastic = (req, res, next) => {
+    let queries = [
+        plasticTankModel.find({})
+    ]
+    
+    Promise.all(queries)
+        .then((result) => {
+            console.log("p");
+            console.log(req.User);
+            res.render('admin/plastic' , {u_name: req.User.adminName, p_data: result[0]})
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+};
 
 module.exports = {
     plastic
